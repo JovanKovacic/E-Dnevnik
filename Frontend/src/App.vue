d18e51493df62a5efc58eb4146773854f29b3431
JovanKovacic <jovankovacic33@gmail.com>
2025-11-20 17:51:43 +0100

    under dev home page

---
+++ Frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home-container">
+    <div class="dots-loader">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+
+    <h1 class="naslov">E-Dnevnik</h1>
+    <p class="status">Under development</p>
+    <p class="autor">Jovan Kovačić</p>
+  </div>
+</template>
+
+<script></script>
